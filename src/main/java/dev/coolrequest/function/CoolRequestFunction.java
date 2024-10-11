1a9bf9682359db41c6b5de53baea18d790ff22d2
houxinlin <houxinlin@seczone.cn>
2024-10-11 09:30:31 +0800

    update CoolRequestFunction.java

--- src/main/java/dev/coolrequest/function/CoolRequestFunction.java
+++ src/main/java/dev/coolrequest/function/CoolRequestFunction.java
@@ -2,4 +2,20 @@ package dev.coolrequest.function;
 
 public class CoolRequestFunction {
 
+    public static class Color {
+
+    }
+
+    public static class Address {
+
+    }
+
+    public static class Phone {
+
+    }
+
+    public static class Date {
+
+    }
+
 }
