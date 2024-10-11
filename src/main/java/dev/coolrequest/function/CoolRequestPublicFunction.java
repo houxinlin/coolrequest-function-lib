db0082cb4bdb4fb2dc62b65915364d74a65edcf9
houxinlin <houxinlin@seczone.cn>
2024-10-11 18:29:44 +0800

    增加randPhoneNumber

--- src/main/java/dev/coolrequest/function/CoolRequestPublicFunction.java
+++ src/main/java/dev/coolrequest/function/CoolRequestPublicFunction.java
@@ -11,7 +11,20 @@ public class CoolRequestPublicFunction {
     }
 
     public static class Phone {
+        private static String[] telFirst = "134,135,136,137,138,139,150,151,152,157,158,159,130,131,132,155,156,133,153".split(",");
 
+        public static String randPhoneNumber() {
+            String first = telFirst[Int.randInt(0, telFirst.length - 1)];
+            String second = String.valueOf(Int.randInt(1, 888) + 10000).substring(1);
+            String third = String.valueOf(Int.randInt(1, 9100) + 10000).substring(1);
+            return first + second + third;
+        }
+    }
+
+    public static class Int {
+        public static int randInt(int start, int end) {
+            return (int) (Math.random() * (end - start + 1) + start);
+        }
     }
 
     public static class Date {
